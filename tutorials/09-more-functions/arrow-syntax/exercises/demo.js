228da8672c45de2f69e4ceaf64008b898cf6c789
lakshmi priya <lakshmipriyaark@gmail.com>
2026-05-22 15:12:48 -0500

    updated file

--- tutorials/09-more-functions/arrow-syntax/exercises/demo.js
+++ tutorials/09-more-functions/arrow-syntax/exercises/demo.js
@@ -10,17 +10,21 @@ function traditionalCalculateFuel(mass) {
 // STAGE 2: Block-Body Arrow Function (Explicit Return)
 
 // DEMO: Rewrite the traditional function with transitional arrow syntax;
-
+const  arrowExplicitCalculateFuel = (mass) => {
+    return mass * 11.2;
+} 
+console.log(`arrowExplicitCalculateFuel ${arrowExplicitCalculateFuel(100)}`);
 // STAGE 3: Concise-Body Arrow Function (Implicit Return)
 
 // DEMO: Rewrite the function again with concise array syntax
+const arrowImplicitCalculateFuel = mass  => mass * 11.2;
 
 // DEMO: Test the concise arrow function
-
+console.log(`${arrowImplicitCalculateFuel(200)}`);
 /** EXAMPLE 2 - THE HOISTING PARADOX **/
 
 // 1. DEMO: On the next line, call the traditional function defined below
-
+console.log (`${readTraditionalSensor()}`);
 function readTraditionalSensor() {
 	return "Telemetry Active";
 }
@@ -30,7 +34,7 @@ function readTraditionalSensor() {
 const readArrowSensor = () => "Telemetry Active";
 
 // DEMO: Call the arrow function again on the next line.
-
+console.log(`${readArrowSensor()}`);
 // DEMO: Make a git commit!
 
 /* 
