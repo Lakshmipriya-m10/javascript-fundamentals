ac731fffa9ce425fe141f2878cff6db1ecdc731b
lakshmi priya <lakshmipriyaark@gmail.com>
2026-05-23 18:47:39 -0500

    objects-intro demo done

--- tutorials/08-objects/objects-intro/exercises/demo.js
+++ tutorials/08-objects/objects-intro/exercises/demo.js
@@ -1,11 +1,34 @@
 /**** INTRODUCTION TO OBJECTS ****/
-
+function heading (clearheading)
+{
+    let clearheadingUpperCase = clearheading.toUpperCase();
+    let titleBoarder = "-----------------------------------";
+    console.log(`${titleBoarder}\n ${clearheading}\n ${titleBoarder}`);
+}
 /** EXAMPLE 1 - DECLARATION & INITIALIZATION **/
-
 // DEMO: Create a structured profile for a ship using Key-Value pairs
-
 // DEMO: Print the object to the console
 
+heading ("Declaration & Initialization")
+
+let shipDetails = {
+  name: "Ocean Explorer",
+  type: "Cargo Ship",
+  speed: 18, 
+  crew: 120,
+  isSailing: true
+};
+console.log(shipDetails);
+
+/*
+Trying to display keys and value separately
+for (let keys in shipDetails)
+{
+    //let allKeys = keys;
+console.log(`${keys}` );
+console.log(`${shipDetails[keys]}`);
+}
+*/
 /** EXAMPLE 2 - THE MEMORY REFERENCE PARADOX **/
 
 const sectorA_Scanner = { oxygenLevel: 98 };
@@ -15,6 +38,11 @@ const sectorB_Scanner = { oxygenLevel: 98 };
 
 const scanner01 = sectorA_Scanner;
 
+heading ("Memory reference paradox")
+
+    console.log(`${sectorA_Scanner === sectorB_Scanner}`);
+
+console.log(`${scanner01 === sectorA_Scanner}`);
 // DEMO: Test if scanner01 is a clone/copy
 
 // DEMO: Make a git commit!
