e433f31c50f02cf25e31286372791d7ffaa7bacc
lakshmi priya <lakshmipriyaark@gmail.com>
2026-05-23 20:42:32 -0500

    Code execution demo-done

--- tutorials/01-basic-syntax/code-execution/exercises/demo.js
+++ tutorials/01-basic-syntax/code-execution/exercises/demo.js
@@ -6,17 +6,25 @@
 
 // DEMO: Re-order the lines below so that there are no reference errors
 // and the total prints as expected.
-console.log(total);
-let total = num1 + num2;
+
+
 let num1 = 3;
 let num2 = 4;
-
+let total = num1 + num2;
+console.log(total);
 /** EXAMPLE 2 - REASSIGNMENT **/
 
 // DEMO: Reassign the value of num multiple times, and use console.log statements.
 // to see those changes reflected at different points along the way.
 
 let num = 1;
+console.log(num);
+ num = 10;
+console.log(num);
+ num = 20;
+console.log(num);
+ num = 78;
+console.log(num);
 
 // DEMO: Make a git commit!
 
